784efd6086db1cbf8c27c76ad106043c6677fa6f
hasanuysaal <hsnuysaal@icloud.com>
2022-09-05 15:05:21 +0300

    add and design tabBarController and Login page

--- SharePhotos/ViewController.swift
+++ SharePhotos/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var passText: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func signInBtn(_ sender: Any) {
+    }
+    
+    @IBAction func signUpBtn(_ sender: Any) {
+        
+        performSegue(withIdentifier: "toFeedVC", sender: nil)
+        
+    }
 }
 
