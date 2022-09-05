05601bd5fdc1ad5bfcd9871427338baac8cacb4f
hasanuysaal <hsnuysaal@icloud.com>
2022-09-05 15:53:00 +0300

    creating user

--- SharePhotos/ViewController.swift
+++ SharePhotos/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
     @IBOutlet weak var emailText: UITextField!
@@ -21,7 +22,32 @@ class ViewController: UIViewController {
     
     @IBAction func signUpBtn(_ sender: Any) {
         
-        performSegue(withIdentifier: "toFeedVC", sender: nil)
+        if emailText.text != "" {
+            if passText.text != "" {
+                
+                Auth.auth().createUser(withEmail: emailText.text!, password: passText.text!) { autDataResult, error in
+                    if error != nil {
+                        self.alertMsg(title: "Error", msg: error?.localizedDescription ?? "User can't be created")
+                    } else {
+                        self.performSegue(withIdentifier: "toFeedVC", sender: nil)
+                    }
+                }
+                
+            } else {
+                alertMsg(title: "Error", msg: "Please enter a password")
+            }
+        } else {
+            alertMsg(title: "Error", msg: "Please enter your e-mail address")
+        }
+        
+    }
+    
+    func alertMsg(title: String, msg: String){
+        
+        let alert = UIAlertController(title: title, message: msg, preferredStyle: .alert)
+        let alertBtn = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        alert.addAction(alertBtn)
+        present(alert, animated: true, completion: nil)
         
     }
 }
