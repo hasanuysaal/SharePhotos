47684da66f05de5667f377dbefc67fe8276b03e0
hasanuysaal <hsnuysaal@icloud.com>
2022-09-05 20:20:49 +0300

    change share view design and choosing photo from user media library

--- SharePhotos/ShareViewController.swift
+++ SharePhotos/ShareViewController.swift
@@ -7,14 +7,37 @@
 
 import UIKit
 
-class ShareViewController: UIViewController {
+class ShareViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var commentTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        imageView.isUserInteractionEnabled = true
+        
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(choosePhoto))
+        imageView.addGestureRecognizer(gestureRecognizer)
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func shareBtn(_ sender: Any) {
+    
+    }
+    
+    @objc func choosePhoto(){
+            
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.sourceType = .photoLibrary
+        present(pickerController, animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image = info[.editedImage] as? UIImage
+        self.dismiss(animated: true)
+    }
 
 }
